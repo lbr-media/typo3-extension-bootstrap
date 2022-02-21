7da9f70344d74af98f4535e12a53d76cce8bc0ff
Marcel <marcel.briefs@lbrmedia.de>
2022-02-21 09:43:11 +0100

    init

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'LBRmedia Bootstrap Template',
+    'description' => 'Provides Twitter Bootstrap 5 and some content elements.',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.5.99',
+            'rte_ckeditor' => '10.4.0-11.5.99',
+            'seo' => '10.4.0-11.5.99'
+        ],
+        'conflicts' => [
+            'css_styled_content' => '*',
+            'fluid_styled_content' => '*',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'LBRmedia\\Bootstrap\\' => 'Classes'
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Marcel Briefs',
+    'author_email' => 'mb@lbrmedia.de',
+    'author_company' => 'private',
+    'version' => '0.0.0-dev',
+];
