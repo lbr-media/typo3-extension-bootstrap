205e351a48f6b3293804a8719c2c1d3da0075ba1
sunixzs <marcel.briefs@lbrmedia.de>
2022-02-24 17:53:27 +0100

    reorganized TypoScript

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,7 +5,23 @@ declare(strict_types=1);
 call_user_func(
     function ($extKey) {
         // add TypoScript
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'General');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript',
+            'General'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript/Content',
+            'Content elements'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript/Styles',
+            'Additional Styles'
+        );
     },
     'bootstrap'
 );
