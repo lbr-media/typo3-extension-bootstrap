f03b8b81787bbaedad40451526cd4e90ce10dc64
sunixzs <marcel.briefs@lbrmedia.de>
2022-10-10 03:48:25 +0200

    Fixed using the TSconfig cropVariants in TCA - Remove TSconfig cropVariants - Set cropVariants as constants in PictureUtility - Fixes #12

--- Classes/Utility/PictureUtility.php
+++ Classes/Utility/PictureUtility.php
@@ -19,6 +19,182 @@ use TYPO3\CMS\Core\Utility\GeneralUtility as CoreGeneralUtility;
 
 class PictureUtility
 {
+    public const ALLOWED_ASPECT_RATIOS = [
+        'NaN' => [
+            'title' => 'frei',
+            'value' => 0.0
+        ],
+        '21:10' => [
+            'title' => '21:10',
+            'value' => 21 / 10
+        ],
+        '21:9' => [
+            'title' => '21:9',
+            'value' => 21 / 9
+        ],
+        '16:10' => [
+            'title' => '16:10',
+            'value' => 16 / 10
+        ],
+        '16:9' => [
+            'title' => '16:9',
+            'value' => 16 / 9
+        ],
+        '9:16' => [
+            'title' => '9:16',
+            'value' => 9 / 16
+        ],
+        '3:4' => [
+            'title' => '3:4',
+            'value' => 3 / 4
+        ],
+        '4:3' => [
+            'title' => '4:3',
+            'value' => 4 / 3
+        ],
+        '2:3' => [
+            'title' => '2:3',
+            'value' => 2 / 3
+        ],
+        '3:2' => [
+            'title' => '3:2',
+            'value' => 3 / 2
+        ],
+        '5:7' => [
+            'title' => '5:7',
+            'value' => 5 / 7
+        ],
+        '7:5' => [
+            'title' => '7:5',
+            'value' => 7 / 5
+        ],
+        '4:5' => [
+            'title' => '4:5',
+            'value' => 4 / 5
+        ],
+        '5:4' => [
+            'title' => '5:4',
+            'value' => 5 / 3
+        ],
+        '1:2' => [
+            'title' => '1:2',
+            'value' => 1 / 2
+        ],
+        '2:1' => [
+            'title' => '2:1',
+            'value' => 2 / 1
+        ],
+        '1:1' => [
+            'title' => '1:1',
+            'value' => 1 / 1
+        ],
+    ];
+
+    public const ALLOWED_ASPECT_RATIOS_SOCIAL_MEDIA = [
+        'NaN' => self::ALLOWED_ASPECT_RATIOS['NaN'],
+        '120:63' => [
+            'title' => '1.91:1 (Facebook)',
+            'value' => 120 / 63
+        ],
+        '2:1' => self::ALLOWED_ASPECT_RATIOS['2:1'],
+        '1:1' => self::ALLOWED_ASPECT_RATIOS['1:1'],
+    ];
+
+    public const ALLOWED_ASPECT_RATIOS_PAGES_MEDIA = [
+        'NaN' => self::ALLOWED_ASPECT_RATIOS['NaN'],
+        '9:16' => [
+            'title' => 'XS (9:16)',
+            'value' => 9 / 16
+        ],
+        '16:9' => [
+            'title' => 'SM-XXL (16:9)',
+            'value' => 16 / 9
+        ],
+    ];
+
+    public const CROP_VARIANTS = [
+        'default' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.default',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'xs' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xs',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'sm' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.sm',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'md' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.md',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'lg' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.lg',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'xl' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xl',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'xxl' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xxl',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'lightbox' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.lightbox',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'navigation' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.navigation',
+            'selectedRatio' => '1:1',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS
+        ],
+        'social' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.social',
+            'selectedRatio' => 'NaN',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_SOCIAL_MEDIA
+        ],
+        'pages_media_xs' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xs',
+            'selectedRatio' => '9:16',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+        'pages_media_sm' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.sm',
+            'selectedRatio' => '16:9',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+        'pages_media_md' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.md',
+            'selectedRatio' => '16:9',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+        'pages_media_lg' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.lg',
+            'selectedRatio' => '16:9',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+        'pages_media_xl' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xl',
+            'selectedRatio' => '16:9',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+        'pages_media_xxl' => [
+            'title' => 'LLL:EXT:bootstrap/Resources/Private/Language/locallang_db.xlf:cropVariants.xxl',
+            'selectedRatio' => '16:9',
+            'allowedAspectRatios' => self::ALLOWED_ASPECT_RATIOS_PAGES_MEDIA
+        ],
+    ];
+
     public const CROP_VARIANTS_BOOTSTRAP = [
         'xs',
         'sm',
@@ -46,42 +222,16 @@ class PictureUtility
     ];
 
     /**
-     * Returns the value of ext_conf_template.txt:tcaCropVariantsOverridePid.
-     */
-    public static function getTcaCropVariantsOverridePid(): int
-    {
-        if (isset($GLOBALS['tx_bootstrap_tcaCropVariantsOverridePid'])) {
-            return (int)$GLOBALS['tx_bootstrap_tcaCropVariantsOverridePid'];
-        }
-
-        $extConf = CoreGeneralUtility::makeInstance(ExtensionConfiguration::class)->get('bootstrap');
-        if (isset($extConf['tcaCropVariantsOverridePid']) && is_numeric($extConf['tcaCropVariantsOverridePid'])) {
-            $GLOBALS['tx_bootstrap_tcaCropVariantsOverridePid'] = (int)$extConf['tcaCropVariantsOverridePid'];
-        } else {
-            $GLOBALS['tx_bootstrap_tcaCropVariantsOverridePid'] = 1;
-        }
-
-        return $GLOBALS['tx_bootstrap_tcaCropVariantsOverridePid'];
-    }
-
-    /**
      * Filters and returns all cropVariants by the given one (enables only the given one).
-     * TODO@sunixzs https://github.com/lbr-media/typo3-extension-bootstrap/issues/12 Adjust to v12
      */
     public static function getTcaCropVariantsOverride(array $enabledCropVariants): array
     {
-        // $pageTsConfig = BackendUtility::getPagesTSconfig(self::getTcaCropVariantsOverridePid());
-
-        $cropVariants = ['default'];
-        // if (
-        //     isset($pageTsConfig['TCEFORM.']['sys_file_reference.']['crop.']['config.']['cropVariants.']) &&
-        //     is_array($pageTsConfig['TCEFORM.']['sys_file_reference.']['crop.']['config.']['cropVariants.'])
-        // ) {
-        //     foreach ($pageTsConfig['TCEFORM.']['sys_file_reference.']['crop.']['config.']['cropVariants.'] as $cropVariant => $config) {
-        //         $cropVariant = rtrim($cropVariant, '.');
-        //         $cropVariants[$cropVariant]['disabled'] = in_array($cropVariant, $enabledCropVariants) ? false : true;
-        //     }
-        // }
+        $cropVariants = [];
+        foreach ($enabledCropVariants as $enabledCropVariant) {
+            if (array_key_exists($enabledCropVariant, self::CROP_VARIANTS)) {
+                $cropVariants[$enabledCropVariant] = self::CROP_VARIANTS[$enabledCropVariant];
+            }
+        }
 
         return $cropVariants;
     }
